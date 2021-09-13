11bf0db76ec7dd62699a001d3490e79ce7ccfae1
fredrik <klosansa1@hotmail.com>
2021-09-13 16:02:42 +0200

    added main layout and added new dependency

--- app/src/main/java/com/example/android_project/MainActivity.kt
+++ app/src/main/java/com/example/android_project/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.android_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
+import java.io.InputStream
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //temp code start fredrik
+        val image_url = "https://lumiere-a.akamaihd.net/v1/images/p_avengersendgame_19751_e14a0104.jpeg?region=0,0,540,810&width=480"
+        val imageView = findViewById<ImageView>(R.id.characterPic)
+        Picasso.get().load(image_url).into(imageView)
+
+
     }
 }
