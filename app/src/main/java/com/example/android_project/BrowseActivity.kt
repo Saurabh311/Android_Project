ef70218701a046f400730efd59bdb5db647b7b15
Coin-byte <kriswern@icloud.com>
2021-09-14 12:55:59 +0200

    Addded recycler view, activity and adapter

---
+++ app/src/main/java/com/example/android_project/BrowseActivity.kt
@@ -0,0 +1,21 @@
+package com.example.android_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android_project.adapter.RecyclerAdapter
+
+class BrowseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_browse)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = RecyclerAdapter(this)
+        TODO("retrieve data and send to RecyclerAdapter(this, -data-)")
+
+
+        //Use this setting to improve performance
+        //if you know changes in content do not change layout size
+    }
+}
