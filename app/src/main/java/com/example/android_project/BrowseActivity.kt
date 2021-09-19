bf47acbf0db94ca77c1d09ce927c50a12c4bf999
Oskar Andersson <oskand01@gmail.com>
2021-09-19 20:25:15 +0200

    adds search function and connects api response to browseActivity

--- app/src/main/java/com/example/android_project/BrowseActivity.kt
+++ app/src/main/java/com/example/android_project/BrowseActivity.kt
@@ -1,12 +1,13 @@
 package com.example.android_project
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android_project.adapter.RecyclerAdapter
 import com.example.android_project.model.CharacterResponse
@@ -22,8 +23,7 @@ class BrowseActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_browse)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-        recyclerView.adapter = RecyclerAdapter(this)
+
         //TODO("retrieve data and send to RecyclerAdapter(this, -data-)")
 
     }
@@ -32,18 +32,57 @@ class BrowseActivity : AppCompatActivity() {
     fun searchForHero(view: View) {
 
         et_searchHero = findViewById(R.id.et_heroSearch)
-
         val query = et_searchHero.text.toString()
-        if(query != "") {
+
+        //Hides the keyboard when user clicks search btn
+        val view = this.currentFocus
+        et_searchHero.clearFocus()
+        if (view != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+
+        if (query != "") {
             et_searchHero.text.clear()
-            //APIclient.searchHeroByName(query)
+            getHeroes(query)
+
         } else {
-            Toast.makeText(this, "Enter at least one character to search for heroes", Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                this,
+                "Enter at least one character to search for heroes",
+                Toast.LENGTH_SHORT
+            ).show()
         }
 
 
-
-
-
     }
+
+    fun getHeroes(query: String) {
+        val client = APIclient.apiService.fetchCharacterByName(query)
+        client.enqueue(object : retrofit2.Callback<CharacterResponse> {
+            override fun onResponse(
+                call: Call<CharacterResponse>,
+                response: Response<CharacterResponse>
+            ) {
+                if (response.isSuccessful) {
+
+                    val result = response.body()?.results
+                    //Log.d("Character: ", "" + result)
+
+                    result?.let {
+                        val adapter = RecyclerAdapter(result)
+                        val recyclerView: RecyclerView = findViewById(R.id.rv_heroList)
+                        recyclerView.adapter = adapter
+                        recyclerView?.layoutManager = LinearLayoutManager(this@BrowseActivity)
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<CharacterResponse>, t: Throwable) {
+                Log.e("failed", "" + t.message)
+            }
+        })
+    }
+
+
 }
